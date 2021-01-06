61dfd6b2aaffaa1a428ea4a960f011800df7b229
MapleGraph <pradumn@queuebuster.co>
2021-01-06 20:15:27 +0530

    new build

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 if (process.env.NODE_ENV == 'production') {
-    app.use(express.static('static/build'));
+    app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
+        console.log('here')
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
