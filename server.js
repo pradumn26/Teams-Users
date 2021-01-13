58d3b40541d96ea3740e0a30d67dfa492e7b91de
MapleGraph <pradumn@queuebuster.co>
2021-01-13 10:55:14 +0530

    pushing to github

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ if (process.env.NODE_ENV == 'production') {
     app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
-        console.log('here')
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
